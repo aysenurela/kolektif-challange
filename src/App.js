6cfd51872ba17c618b1963c7e3bcc34ef53dcd14
Aveleira <elaaysenur@hotmail.com>
2020-01-20 15:24:32 +0300

    posts and their comments added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { posts } from './data/posts'
+import { comments } from './data/comments'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () { 
+    const _posts = posts
+    const _comments = comments
+      return (
+        <div className="mainContainer">
+          {_posts.map((post)=>{
+            const {
+              userId,
+              id,
+              title,
+              body
+            } = post
+            return (
+              <div key={id} className="postContainer">
+                <div className="user_title_container">
+                  <div className="user"> {`user_${userId}`} </div>
+                  <div className="postTitle">{title}</div>
+                </div>  
+                <div className="postBody">{body}</div>
+                <div>
+                  {
+                    _comments.map((comment)=>{
+                      if (comment.postId === id) {
+                        return (
+                          <div key={comment.id} className="comment">
+                            <div>{comment.body}</div>
+                            <div className="comment_user_information">
+                              <div>{comment.name}</div>
+                              <div>{comment.email}</div>
+                            </div>
+                          </div>
+                        )
+                      }
+                    })
+                  }
+                </div>
+              </div>
+            )
+          })
+          }
+        </div>
+    )
+  }
 }
 
 export default App;
